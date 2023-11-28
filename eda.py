defce962b7873ffbc949460724c215106d79fb1a
jakub-czerniak <j.deadyet@protonmail.com>
2023-11-28 19:05:19 +0100

    Remove key

--- eda.py
+++ eda.py
@@ -8,11 +8,10 @@ df = pd.read_csv('Spotify_Youtube.csv', index_col=0)
 df = df.drop(['Url_spotify', 'Uri', 'Url_youtube', 'Description', 'Channel'], axis=1)
 
 # missing values
+print(df.nunique())
 df.info()
-df.isna().sum()
+print(df.isnull().sum())
 df = df.dropna()
-df.info()
-df.isna().sum()
 
 # replace string with int
 df['Licensed'] = df['Licensed'].astype(int)
@@ -20,12 +19,10 @@ df['official_video'] = df['official_video'].astype(int)
 df['Album_type'] = df['Album_type'].replace('album', 0)
 df['Album_type'] = df['Album_type'].replace('single', 1)
 df['Album_type'] = df['Album_type'].replace('compilation', 2)
-print(df.Album_type.unique())
 # correlation
-df_corr = df.drop(['Artist', 'Track', 'Album', 'Album_type', 'Title'], axis=1)
+df_corr = df.drop(['Artist', 'Track', 'Album', 'Album_type', 'Title', 'Key'], axis=1)
 corrM = df_corr.corr(method='pearson')
-print(corrM)
-plt.figure(figsize=(20, 9))
+plt.figure(figsize=(9, 20))
 heatmap = sns.heatmap(corrM, vmin=-1, vmax=1, annot=True, cmap='BrBG')
 heatmap.set_title('Macierz korelacji')
 plt.savefig('heatmap.png', dpi=300, bbox_inches='tight')
