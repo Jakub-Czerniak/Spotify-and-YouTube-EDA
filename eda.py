6cd94331453ba01c67163cfad1df1046dd74765e
jakub-czerniak <j.deadyet@protonmail.com>
2023-11-29 18:58:10 +0100

    Drop duplicates

--- eda.py
+++ eda.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('Spotify_Youtube.csv', index_col=0)
 
 # drop unnecessary columns
-df = df.drop(['Url_spotify', 'Uri', 'Url_youtube', 'Description', 'Channel'], axis=1)
+df = df.drop(['Url_spotify', 'Url_youtube', 'Description', 'Channel'], axis=1)
 
 # missing values
 print(df.nunique())
@@ -13,14 +13,24 @@ df.info()
 print(df.isnull().sum())
 df = df.dropna()
 
+
+
 # replace string with int
 df['Licensed'] = df['Licensed'].astype(int)
 df['official_video'] = df['official_video'].astype(int)
 df['Album_type'] = df['Album_type'].replace('album', 0)
 df['Album_type'] = df['Album_type'].replace('single', 1)
 df['Album_type'] = df['Album_type'].replace('compilation', 2)
+df.info()
+#drop duplicates
+df = df.sort_values('Stream', ascending=False)
+df = df.drop_duplicates(subset='Uri', keep='first')
+
+df.info()
+
+print(df.nunique())
 # correlation
-df_corr = df.drop(['Artist', 'Track', 'Album', 'Album_type', 'Title', 'Key'], axis=1)
+df_corr = df.drop(['Artist', 'Track', 'Album', 'Album_type', 'Title', 'Key', 'Uri'], axis=1)
 corrM = df_corr.corr(method='pearson')
 plt.figure(figsize=(9, 20))
 heatmap = sns.heatmap(corrM, vmin=-1, vmax=1, annot=True, cmap='BrBG')
