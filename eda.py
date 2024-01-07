0cd472f883d9a6afb2dcb13545f364749c3ca753
jakub-czerniak <j.deadyet@protonmail.com>
2024-01-07 15:31:27 +0100

    Dummies from ordinal and association rules

--- eda.py
+++ eda.py
@@ -3,6 +3,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from math import log10
 import numpy as np
+from mlxtend.frequent_patterns import fpgrowth
+from mlxtend.frequent_patterns import apriori
+
 
 df = pd.read_csv('Spotify_Youtube.csv', index_col=0)
 
@@ -15,14 +18,14 @@ df_start.rename(columns={'Danceability':'Taneczność', 'Energy':'Energia', 'Lou
 df = df.dropna()
 
 
-# replace string with int
+'''# replace string with int
 df['Licensed'] = df['Licensed'].astype(int)
 df['official_video'] = df['official_video'].astype(int)
 df['Album_type'] = df['Album_type'].replace('album', 0)
 df['Album_type'] = df['Album_type'].replace('single', 1)
 df['Album_type'] = df['Album_type'].replace('compilation', 2)
 # drop duplicates
-df = df.drop_duplicates(subset='Uri', keep='first')
+df = df.drop_duplicates(subset='Uri', keep='first')'''
 
 # correlation
 '''df_corr = df.copy()
@@ -61,6 +64,7 @@ fig.savefig('scaYT.png', dpi=300, bbox_inches='tight')
 
 # conversion to ordinal data
 df_ordinal = df.copy()
+df_ordinal = df_ordinal.drop(['Title', 'Uri', 'Album', 'Track', 'Artist'], axis=1)
 df_ordinal.rename(columns={'Danceability': 'Taneczność', 'Energy': 'Energia', 'Loudness': 'Głośność',
                            'Speechiness': 'Tekstowość', 'Acousticness': 'Akustyczność',
                            'Instrumentalness': 'Instrumentalność', 'Liveness': 'Żywość',
@@ -70,24 +74,17 @@ df_ordinal.rename(columns={'Danceability': 'Taneczność', 'Energy': 'Energia', 
                            'official_video': 'Oficjalny film'}, inplace=True)
 
 
-def convert_numeric01_to_ordinal15(df, column_name):
-    # converts numeric 0.0-1.0 data to ordinal 1-5 data
-    df[column_name] = df[column_name].apply(lambda x: int(x//0.2))
-    df[column_name].replace({0: 1, 1: 2, 2: 3, 3: 4, 4: 5})
-    return df
-
-
 def range_to_1_5(x, ranges):
     if ranges[4][0] <= x <= ranges[4][1]:
-        return 5
+        return '5'
     if ranges[3][0] <= x < ranges[3][1]:
-        return 4
+        return '4'
     if ranges[2][0] <= x < ranges[2][1]:
-        return 3
+        return '3'
     if ranges[1][0] <= x < ranges[1][1]:
-        return 2
+        return '2'
     if ranges[0][0] <= x < ranges[0][1]:
-        return 1
+        return '1'
 
 
 # pd.options.display.float_format = '{:.2f}'.format # uncomment to print ranges without scientific notation
@@ -118,23 +115,35 @@ df_ordinal = convert_numeric_to_ordinal(df_ordinal, 'Tempo')
 
 
 df_ordinal['Tekstowość'] = df_ordinal['Tekstowość'].mask(df_ordinal['Tekstowość'] < 0.13, 0)
+df_ordinal['Tekstowość'] = df_ordinal['Tekstowość'].mask(df_ordinal['Tekstowość'] > 0.98, 3)
 df_ordinal['Tekstowość'] = df_ordinal['Tekstowość'].mask(df_ordinal['Tekstowość'].between(0.13, 0.66), 1)
 df_ordinal['Tekstowość'] = df_ordinal['Tekstowość'].mask(df_ordinal['Tekstowość'].between(0.66, 0.98), 2)
-df_ordinal['Tekstowość'] = df_ordinal['Tekstowość'].mask(df_ordinal['Tekstowość'] > 0.98, 3)
+df_ordinal['Tekstowość'] = df_ordinal['Tekstowość'].astype(int).astype(str)
+df_ordinal['Tekstowość'] = df_ordinal['Tekstowość'].astype('category')
+
 
 df_ordinal['Instrumentalność'] = df_ordinal['Instrumentalność'].mask(df_ordinal['Instrumentalność'] <= 0.5, 0)
 df_ordinal['Instrumentalność'] = df_ordinal['Instrumentalność'].mask(df_ordinal['Instrumentalność'] > 0.5, 1)
+df_ordinal['Instrumentalność'] = df_ordinal['Instrumentalność'].astype(int).astype(str)
+df_ordinal['Instrumentalność'] = df_ordinal['Instrumentalność'].astype('category')
+
 
 df_ordinal['Żywość'] = df_ordinal['Żywość'].mask(df_ordinal['Żywość'] <= 0.8, 0)
 df_ordinal['Żywość'] = df_ordinal['Żywość'].mask(df_ordinal['Żywość'] > 0.8, 1)
+df_ordinal['Żywość'] = df_ordinal['Żywość'].astype(int).astype(str)
+df_ordinal['Żywość'] = df_ordinal['Żywość'].astype('category')
 
-df_ordinal['Czas trwania'] = df_ordinal['Czas trwania'] = df_ordinal['Czas trwania'].apply(lambda x: x//(1000*60))
+df_ordinal['Key'] = df_ordinal['Key'].astype(int).astype(str)
+df_ordinal['Key'] = df_ordinal['Key'].astype('category')
+
+df_ordinal['Czas trwania'] = df_ordinal['Czas trwania'] = df_ordinal['Czas trwania'].apply(lambda x: str(int(x//(1000*60))))
+df_ordinal['Czas trwania'] = df_ordinal['Czas trwania'].astype('category')
 
 
 def convert_numeric_to_ordinal_log(df, column_name):
     # converts numeric values to log10 rounded downwards
     df = df.loc[(df[column_name]!=0)]
-    df[column_name] = df[column_name].apply(lambda x: int(log10(x)//1))
+    df[column_name] = df[column_name].apply(lambda x: str(int(log10(x)//1)))
     return df
 
 
@@ -146,8 +155,13 @@ df_ordinal = convert_numeric_to_ordinal_log(df_ordinal, 'Komentarze')
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     print(df_ordinal.head())
 
-max_ods = df_ordinal['Odsłuchania'].max()
-print('Maksymalny log liczby odsłuchań: ' + str(max_ods))
-print('Liczba utworów z milardowymi odsłuchaniami: ' + str(df_ordinal['Odsłuchania'].value_counts()[max_ods]))
+
+df_dummies = pd.get_dummies(df_ordinal, columns= ['Album_type', 'Taneczność', 'Energia', 'Key', 'Głośność', 'Tekstowość',
+                                                  'Akustyczność', 'Instrumentalność', 'Żywość', 'Pozytywność', 'Tempo',
+                                                  'Czas trwania', 'Wyświetlenia', 'Polubienia', 'Komentarze', 'Odsłuchania'])
 
 
+
+fp_results = fpgrowth(df_dummies, min_support=0.5, use_colnames=True, max_len=None, verbose=0)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(fp_results)
